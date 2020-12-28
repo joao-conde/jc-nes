7d236acd37fb0166fe0963ebd1dfb4b55346253b
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2020-12-28 23:14:48 +0000

    Bus start

--- src/bus.rs
+++ src/bus.rs
@@ -1,3 +1,18 @@
+#[derive(Debug)]
 pub struct Bus {
 
+}
+
+impl Bus {
+    pub fn connect(&self) {
+        
+    }
+
+    pub fn read(&self) {
+
+    }
+
+    pub fn write(&self) {
+
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,8 @@ use jc_nes::bus::Bus;
 fn main() {
     println!("Booting up NES...");
     let bus = Bus {};
+    bus.connect();
+    bus.read();
+    bus.write();
+    println!("{:?}", bus)
 }
