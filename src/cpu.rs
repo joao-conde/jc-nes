ea99772a0dd1346bf5d5e44e86e958cc698b018b
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2020-12-30 01:52:34 +0000

    Instruction setup

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,4 +1,7 @@
+use std::ops::Add;
+
 use super::bus::{Bus, Device};
+
 pub struct CPU<'a> {
     a: u8,
     x: u8,
@@ -16,11 +19,11 @@ impl<'a> CPU<'a> {
             a: 0x00,
             x: 0x00,
             y: 0x00,
-            pc: 0x00,
+            pc: 0xC000,
             sp: 0x00,
             sr: 0x00,
             cycles_left: 0,
-            bus: bus
+            bus: bus,
         }
     }
 
@@ -28,10 +31,24 @@ impl<'a> CPU<'a> {
         if self.cycles_left == 0 {
             let opcode = self.bus.read(self.pc);
             self.pc += 1;
-
         }
         self.cycles_left -= 1;
     }
+
+    pub fn instruction_from_byte(&self, byte: u8) -> Instruction {
+        match byte {
+            0xA9 => Instruction::new(CPU::lda, CPU::imm, 2, 2),
+            _ => unreachable!(),
+        }
+    }
+
+    // addressing modes
+    pub fn imm(cpu: &CPU, literal: u8) -> u8 {
+        literal
+    }
+
+    // opcodes
+    pub fn lda(cpu: &CPU, val: u8) {}
 }
 
 impl<'a> Device for CPU<'a> {
@@ -45,8 +62,28 @@ impl<'a> Device for CPU<'a> {
     }
 }
 
-struct Instruction {
-    cycles: u8,
-    address_mode_fn: String,
-    opcode_fn: String
+type AddressModeFn = fn(&CPU, u8) -> u8;
+type OpcodeFn = fn(&CPU, u8);
+
+pub struct Instruction {
+    opcode_fn: OpcodeFn,
+    address_mode_fn: AddressModeFn,
+    size: u8,
+    duration: u8,
+}
+
+impl Instruction {
+    pub fn new(
+        opcode_fn: OpcodeFn,
+        address_mode_fn: AddressModeFn,
+        duration: u8,
+        size: u8,
+    ) -> Instruction {
+        Instruction {
+            opcode_fn,
+            address_mode_fn,
+            size,
+            duration,
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use jc_nes::bus::Bus;
 use jc_nes::cpu::CPU;
 use jc_nes::ram::RAM;
+use std::fs::File;
+use std::io::Read;
 
 fn main() {
     println!("Booting up NES...");
@@ -9,9 +11,20 @@ fn main() {
 
     let cpu = CPU::new(&bus);
 
-    
-    let ram = RAM { mem: [0; 64 * 1024] };
+    let mut rom = File::open("roms/nestest.nes").unwrap();
+    let mut buffer = vec![0; 64 * 1024];
+
+    rom.read(&mut buffer).expect("buffer overflow");
+    let ram = RAM {
+        mem: [0; 64 * 1024],
+    };
 
     bus.connect(0x0000..=0xFFFF, ram);
 
+    println!(
+        "{:0b} {:0b} {:0b}",
+        0b11110000,
+        0b11110000 & 0b1111,
+        0b11110000 >> 4
+    );
 }
