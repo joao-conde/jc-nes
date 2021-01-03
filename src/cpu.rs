435f8854d422a5e3535d0797ddc1b2ffb8450c1f
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-01-03 20:08:59 +0000

    Fixes on cmp instructions

--- src/cpu.rs
+++ src/cpu.rs
@@ -499,7 +499,7 @@ impl<'a> CPU<'a> {
         let operand = self.read(address);
         self.set_flag(Flag::Carry, self.a >= operand);
         self.set_flag(Flag::Zero, self.a == operand);
-        self.set_flag(Flag::Negative, self.a > operand);
+        self.set_flag(Flag::Negative, (self.a.wrapping_sub(operand) & 0x80) >> 7 == 1);
         self.pc += 1;
     }
 
@@ -507,7 +507,7 @@ impl<'a> CPU<'a> {
         let operand = self.read(address);
         self.set_flag(Flag::Carry, self.x >= operand);
         self.set_flag(Flag::Zero, self.x == operand);
-        self.set_flag(Flag::Negative, self.x > operand);
+        self.set_flag(Flag::Negative, (self.x.wrapping_sub(operand) & 0x80) >> 7 == 1);
         self.pc += 1;
     }
 
@@ -515,7 +515,7 @@ impl<'a> CPU<'a> {
         let operand = self.read(address);
         self.set_flag(Flag::Carry, self.y >= operand);
         self.set_flag(Flag::Zero, self.y == operand);
-        self.set_flag(Flag::Negative, self.y > operand);
+        self.set_flag(Flag::Negative, (self.y.wrapping_sub(operand) & 0x80) >> 7 == 1);
         self.pc += 1;
     }
 
