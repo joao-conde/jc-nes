f07afc5451bff70c612c3c3545e00900d3ff6369
unknown <joaodiasconde@gmail.com>
2021-02-02 15:04:04 +0000

    feat: paint pattern table

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ use jc_nes::ram::RAM;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use sdl2::rect::Point;
-use std::cell::RefCell;
+use core::panic;
+use std::{cell::RefCell, ops::Deref};
 use std::fs::File;
 use std::io::Read;
 use std::rc::Rc;
@@ -20,31 +21,28 @@ fn dev() {
     let mut file = File::open(rom_path).unwrap();
     let mut rom = Vec::new();
     file.read_to_end(&mut rom).unwrap();
+    
     // skip header (16 bytes)
     let mut bytes = rom.bytes().skip(16);
     // need to copy this to CPU RAM:
     let _prg_mem = bytes
         .by_ref()
-        .take(16 * 1024)
+        .take(32 * 1024)
         .flatten()
         .collect::<Vec<u8>>(); // 16kB per bank
     let char_mem = bytes.by_ref().take(8 * 1024).flatten().collect::<Vec<u8>>(); // 8kB per bank
 
-    let mut vram = vec![0u8; 64 * 1024]; // 64kB VRAM
-    vram[0x0000..0x2000].clone_from_slice(&char_mem);
-    let vram = Rc::new(RefCell::new(RAM { mem: vram }));
-
-    let mut bus = Bus::default();
-    bus.connect(0x0000..=0xFFFF, &vram);
-
-    let width = 128;
-    let height = 256;
+    let mut vram_mem = vec![0u8; 64 * 1024]; // 64kB VRAM
+    &vram_mem[0x0000..0x2000].clone_from_slice(&char_mem);
+    
+    let width: usize = 128;
+    let height: usize = 256;
 
     let sdl = sdl2::init().unwrap();
     let video_subsystem = sdl.video().unwrap();
 
     let window = video_subsystem
-        .window("Pattern Table", width, height)
+        .window("Pattern Table", 3 *width as u32, 3 * height as u32)
         .resizable()
         .build()
         .unwrap();
@@ -52,21 +50,68 @@ fn dev() {
     let mut canvas = window.into_canvas().build().unwrap();
 
     canvas.clear();
-    for r in 0..height {
-        for c in 0..width {
-            let addr = (r / 8 * 0x100) + (r % 8) + (c / 8) * 0x10;
-            let v1 = bus.read(addr as u16).unwrap();
-            let v2 = bus.read(addr as u16 + 8).unwrap();
-            let pixel = ((v1 >> (7 - (c % 8))) & 0b1) + ((v2 >> (7 - (c % 8))) & 0b1) * 2;
+    
+    const TILE_PIXEL_WIDTH: usize = 8;
+    const TILE_PIXEL_HEIGHT: usize = TILE_PIXEL_WIDTH;
+    const TILE_BYTE_WIDTH: usize = 2 * TILE_PIXEL_WIDTH;
+
+    let mut count_x = 1;
+    let mut count_y = 1;
+    for y in 0..height {
+        for x in 0..width {
+            let tile_x = x / TILE_PIXEL_WIDTH;
+            let tile_y = y / TILE_PIXEL_HEIGHT;
+            let addr = tile_y * height + tile_x * TILE_BYTE_WIDTH + (y % 8);
+
+            let mut lsb: u8 = vram_mem[addr];
+            let mut msb: u8 = vram_mem[addr + 8];
+
+            let mut pixel_help: u16 = 0x0000;
+
+            for i in 0..8 {
+                let bit0: u8 = lsb & 0x01;
+                let bit1: u8 = msb & 0x01;
+
+                pixel_help |= (bit0 as u16) << (i * 2);
+                pixel_help |= (bit1 as u16) << (i * 2 + 1);
+
+                lsb >>= 1;
+                msb >>= 1;
+            }
+            
+            let pos = 7 - (x % 8);
+            let opt = pos * 2;
+            let pixel = (pixel_help & (0x3 << opt)) >> opt;
+            // let pix_str = &format!("{}", pixel);
+            // let pixel_ascii = if pixel == 0 { " " } else { pix_str };
+            // print!("{} ", pixel_ascii);
+
             match pixel {
-                0 => canvas.set_draw_color(Color::RGB(128, 179, 255)),
+                0 => canvas.set_draw_color(Color::RGB(0, 0, 0)),
                 1 => canvas.set_draw_color(Color::RGB(0, 102, 255)),
                 2 => canvas.set_draw_color(Color::RGB(0, 51, 128)),
                 3 => canvas.set_draw_color(Color::RGB(0, 10, 26)),
-                _ => canvas.set_draw_color(Color::RGB(0, 0, 0)),
+                _ => panic!("unexpected pixel value"),
             }
-            canvas.draw_point(Point::new(c as i32, r as i32)).unwrap();
+
+            for i in 0..3 {
+                for j in 0..3 {
+                    canvas.draw_point(
+                        Point::new(i + 3 * x as i32, j + 3 * y as i32)
+                    ).unwrap();
+                }
+            }
+            
+            if count_x % 8 == 0 {
+                print!(" ");
+            }
+            count_x += 1;
         }
+        if count_y % 8 == 0 {
+            print!("\n");
+        }
+        count_y += 1;
+        print!("\n");
     }
 
     canvas.present();
