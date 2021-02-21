53ee9485d653e9b641d135b3ca981b82801e1059
unknown <joaodiasconde@gmail.com>
2021-02-21 14:32:33 +0000

    fix vramaddress struct bug

--- src/ppu/vram_address.rs
+++ src/ppu/vram_address.rs
@@ -1,20 +1,20 @@
 #[derive(Clone, Copy, Debug, Default)]
-pub(in crate::ppu) struct VRAMAddress {
-    pub(in crate::ppu) coarse_x: u8,
-    pub(in crate::ppu) coarse_y: u8,
-    pub(in crate::ppu) nametable_x: u8,
-    pub(in crate::ppu) nametable_y: u8,
-    pub(in crate::ppu) fine_y: u8,
+pub struct VRAMAddress {
+    pub coarse_x: u8,
+    pub coarse_y: u8,
+    pub nametable_x: u8,
+    pub nametable_y: u8,
+    pub fine_y: u8,
 }
 
 impl From<u16> for VRAMAddress {
     fn from(word: u16) -> VRAMAddress {
         VRAMAddress {
             coarse_x: (word & 0x001F) as u8,
-            coarse_y: (word & 0x03E0) as u8,
-            nametable_x: (word & 0x0400) as u8,
-            nametable_y: (word & 0x0800) as u8,
-            fine_y: (word & 0x7000) as u8,
+            coarse_y: ((word & 0x03E0) >> 5) as u8,
+            nametable_x: ((word & 0x0400) >> 10) as u8,
+            nametable_y: ((word & 0x0800) >> 11) as u8,
+            fine_y: ((word & 0x7000) >> 12) as u8,
         }
     }
 }
